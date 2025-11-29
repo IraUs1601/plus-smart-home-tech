c0c0e28a3737f3c05f06d94364bc53db66d2b2a7
Irina Uskova <irinus1601@gmail.com>
2025-11-29 21:31:55 +0100

    add event enums

---
+++ telemetry/collector/src/main/java/ru/yandex/practicum/kafka/telemetry/collector/model/ConditionType.java
@@ -0,0 +1,10 @@
+package ru.yandex.practicum.kafka.telemetry.collector.model;
+
+public enum ConditionType {
+    MOTION,
+    LUMINOSITY,
+    SWITCH,
+    TEMPERATURE,
+    CO2LEVEL,
+    HUMIDITY
+}
