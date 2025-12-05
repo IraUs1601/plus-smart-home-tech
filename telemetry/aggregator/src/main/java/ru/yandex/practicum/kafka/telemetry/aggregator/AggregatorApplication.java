eb4b5e700b41e17b78022be5d04ece791cfb575d
Irina Uskova <irinus1601@gmail.com>
2025-12-05 19:20:05 +0100

    chore: small updates before submission

---
+++ telemetry/aggregator/src/main/java/ru/yandex/practicum/kafka/telemetry/aggregator/AggregatorApplication.java
@@ -0,0 +1,14 @@
+package ru.yandex.practicum.kafka.telemetry.aggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+
+@SpringBootApplication(exclude = KafkaAutoConfiguration.class)
+@ConfigurationPropertiesScan
+public class AggregatorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AggregatorApplication.class, args);
+    }
+}
