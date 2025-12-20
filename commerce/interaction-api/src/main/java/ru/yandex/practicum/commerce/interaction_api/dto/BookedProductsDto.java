3016e12f28eab5ce52fd8206adc3ff4f146ebed6
Irina Uskova <irinus1601@gmail.com>
2025-12-20 16:00:10 +0100

    extend shared DTOs, enums and exceptions

---
+++ commerce/interaction-api/src/main/java/ru/yandex/practicum/commerce/interaction_api/dto/BookedProductsDto.java
@@ -0,0 +1,20 @@
+package ru.yandex.practicum.commerce.interaction_api.dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = lombok.AccessLevel.PRIVATE)
+public class BookedProductsDto {
+    @NotNull
+    Double deliveryWeight;
+    @NotNull
+    Double deliveryVolume;
+    @NotNull
+    Boolean fragile;
+}
---
+++ commerce/interaction-api/src/main/java/ru/yandex/practicum/commerce/interaction_api/dto/OrderDto.java
@@ -0,0 +1,33 @@
+package ru.yandex.practicum.commerce.interaction_api.dto;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import ru.yandex.practicum.commerce.interaction_api.enums.OrderState;
+
+import java.util.Map;
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class OrderDto {
+    UUID orderId;
+    UUID shoppingCartId;
+    Map<String, Long> products;
+    UUID paymentId;
+    UUID deliveryId;
+    OrderState state;
+    Double deliveryWeight;
+    Double deliveryVolume;
+    Boolean fragile;
+    Double totalPrice;
+    Double deliveryPrice;
+    Double productPrice;
+    AddressDto deliveryAddress;
+}
