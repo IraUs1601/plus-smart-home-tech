2219f52bb64b2f8367826dd0f3791177133b0029
Irina Uskova <irinus1601@gmail.com>
2025-12-20 15:53:35 +0100

    add delivery API contract

---
+++ commerce/interaction-api/src/main/java/ru/yandex/practicum/commerce/interaction_api/dto/DeliveryDto.java
@@ -0,0 +1,29 @@
+package ru.yandex.practicum.commerce.interaction_api.dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import ru.yandex.practicum.commerce.interaction_api.enums.DeliveryState;
+
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class DeliveryDto {
+    UUID deliveryId;
+    @NotNull
+    AddressDto fromAddress;
+    @NotNull
+    AddressDto toAddress;
+    @NotNull
+    UUID orderId;
+    @NotNull
+    DeliveryState deliveryState;
+}
